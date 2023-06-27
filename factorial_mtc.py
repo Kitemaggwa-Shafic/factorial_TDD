afd35525db25d2b495126ac511fb62fa786f8389
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-06-27 07:49:15 +0300

    TDD ASSIGNMENT FACTORIAL This is factorial assignment in TDD

---
+++ factorial_mtc.py
@@ -0,0 +1,44 @@
+"""
+FACTORIAL ASSIGNMENT
+1. Shafic Kitemaggwa
+2. Nicholas Edgar Kiwanuka
+"""
+import math
+
+# Using the MTC first principles of calculating factorial of a number
+
+
+# We defined function test_one_1() as below
+def test_one_1():
+    """Function calculating 1!"""
+    number = 1
+    # factorial = math.factorial(1)
+    factorial = math.factorial(number)
+    assert factorial == 1
+
+
+def test_one_2():
+    """Function calculating 2!"""
+    number = 2
+    factorial = math.factorial(number)
+    assert factorial == 2
+
+
+def test_one_3():
+    """Function calculating 3!"""
+    number = 3
+    factorial = math.factorial(number)
+    assert factorial == 6
+
+
+def test_one_4():
+    """Function calculating 4!"""
+    number = 4
+    factorial = math.factorial(number)
+    assert factorial == 24
+
+
+# 1. Running all tests with pytest, they pass because the assert factorial values match
+# with the factorial of the declared numbers
+# 2. All file code is reformetted with black
+# 3. All file code rate at 10.00/10 by pylint
---
+++ test_last_factorial.py
@@ -0,0 +1,39 @@
+"""
+FACTORIAL ASSIGNMENT
+1. Shafic Kitemaggwa
+2. Nicholas Edgar Kiwanuka
+"""
+
+
+# class AddNumbers(unittest.TestCase):
+def test_factorial():
+    """Function calculating the factorial of a declared number"""
+    number = 4
+    result = my_number(number)
+    assert my_number(result)
+
+
+# function for my_number
+def my_first_func():
+    """This function will do nothing just to pass and test it"""
+    # pass
+
+
+# 1st test
+#  When you run the test it FAILS, cause you have no return result specified
+
+
+# ========================================
+# modifying the function above to pass
+def my_number(number):
+    """Using an if and else to try test with number and calc its factorial
+    by subtracting one till its 0"""
+    if number == 0:
+        return 1
+    return number * my_number(number - 1)
+
+
+# 1. Running with pytest, the test pass because the assert value is 24
+#  and is the factorial of number 4 declared
+# 2. All file code is reformetted with black
+# 3. All file code rate at 10.00/10 by pylint
